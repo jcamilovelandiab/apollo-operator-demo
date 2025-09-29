d5ca8829679c845c8630c2e7df7b954f53514548
jonathanrainer <jonathan.rainer@apollographql.com>
2025-09-29 13:10:02 +0100

    Add safety to bootstrap and fix service

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,7 @@ echo ""
 
 echo "Installing ArgoCD"
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl rollout status -n argocd statefulset argocd-application-controller
 read -rp "Press enter to continue"
 echo ""
 
